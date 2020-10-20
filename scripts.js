1fa4c6cdc703ac8711ab3eeb2fe981ff0cde7490
Joan Henry <jhenry081@gmail.com>
2020-10-20 16:17:08 +0300

    Added JS to site for added functionality

--- scripts.js
+++ scripts.js
@@ -1,14 +1,35 @@
 var lisimu = document.querySelector("#lisimu");
+var themeButtons = document.querySelector("#themeButtons");
+var fb = document.getElementById("fb");
+var whatsapp = document.getElementById("whatsapp");
 
-function changeThemeTo(theme){
-    
+function changeThemeTo(theme) {
+  // Remove all themes except for default theme
+  lisimu.classList.remove("whatsapp", "fb");
+
+  // Add theme chosen when button clicked
+  if (theme === "fb") {
+    whatsapp.classList.remove("active");
+    lisimu.classList.add(theme);
+    fb.classList.add("active");
+  } else if (theme === "whatsapp") {
+    fb.classList.remove("active");
+    lisimu.classList.add(theme);
+    whatsapp.classList.add("active");
+  } else {
+    console.log("The theme does not exist");
+  }
 }
 
 var darkMode = false;
-function toggleDarkMode(e){
-    
+function toggleDarkMode(e) {
+  var button = document.getElementById("darkModeButton");
+  button.classList.toggle("on");
+  lisimu.classList.toggle("dark-mode");
 }
 
-function takeScreenshot(){
-    
+function takeScreenshot() {
+  html2canvas(lisimu).then((canvas) => {
+    document.getElementById("download").appendChild(canvas);
+  });
 }
