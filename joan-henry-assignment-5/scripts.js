c05148e5ce0f156b069e7383904dee3ae308cc59
Joan Henry <jhenry081@gmail.com>
2020-10-21 15:37:37 +0300

    Added script for the site

--- joan-henry-assignment-5/scripts.js
+++ joan-henry-assignment-5/scripts.js
@@ -6,15 +6,14 @@ var whatsapp = document.getElementById("whatsapp");
 function changeThemeTo(theme) {
   // Remove all themes except for default theme
   lisimu.classList.remove("whatsapp", "fb");
+  lisimu.classList.add(theme);
 
   // Add theme chosen when button clicked
   if (theme === "fb") {
     whatsapp.classList.remove("active");
-    lisimu.classList.add(theme);
     fb.classList.add("active");
   } else if (theme === "whatsapp") {
     fb.classList.remove("active");
-    lisimu.classList.add(theme);
     whatsapp.classList.add("active");
   } else {
     console.log("The theme does not exist");
