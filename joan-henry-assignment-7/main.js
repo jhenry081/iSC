4b3d6e8b58ccfd0cd858a75b413b606a3ec13b63
Joan Henry <jhenry081@gmail.com>
2020-11-10 15:56:12 +0300

    add onclick handler to each thumbnail image

--- joan-henry-assignment-7/main.js
+++ joan-henry-assignment-7/main.js
@@ -5,11 +5,16 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
-const newImage = document.createElement("img");
 
 for (let i = 1; i < 6; i++) {
+  const newImage = document.createElement("img");
   newImage.setAttribute("src", `images/pic${i}.jpg`);
   thumbBar.appendChild(newImage);
+
+  newImage.onclick = function(){
+    let attribute = newImage.getAttribute("src");
+    displayedImage.setAttribute("src", attribute);
+  }
 }
 
 /* Wiring up the Darken/Lighten button */
