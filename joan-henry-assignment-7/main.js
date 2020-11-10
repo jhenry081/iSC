779cdfa090714b006a7a0ce9b1e650182d66638b
Joan Henry <jhenry081@gmail.com>
2020-11-10 15:39:09 +0300

    looping through the images

--- joan-henry-assignment-7/main.js
+++ joan-henry-assignment-7/main.js
@@ -6,7 +6,10 @@ const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
 const newImage = document.createElement("img");
-newImage.setAttribute("src", xxx);
-thumbBar.appendChild(newImage);
+
+for (let i = 1; i < 6; i++) {
+  newImage.setAttribute("src", `images/pic${i}.jpg`);
+  thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
